3a11dffe54f4bd8cb613bb68094e44ac65619df4
hari-govind-developer <hgovinds06@gmail.com>
2022-12-01 12:01:02 +0530

    calculator v1

---
+++ calc.js
@@ -0,0 +1,12 @@
+function display(num){
+result.value+=num
+}
+function allClr(){
+    result.value=""
+}
+function equall(){
+    result.value=eval(result.value)
+}
+function del(){
+    result.value=result.value.slice(0,-1)
+}
